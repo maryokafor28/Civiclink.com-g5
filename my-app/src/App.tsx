5350c4b68e523ecc30db434c3e7b3f12de35a168
maryokafor28 <vincyokafor@gmail.com>
2025-04-11 16:01:58 +0100

    searchofficials-emptyspace

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import FilterPanel from './components/Filterpanel';
+import FilterPanel from './components/Filterpanel';
 import CommissionerList from './components/Searchpage'
+import SearchOfficials from './components/Searcofficials';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* { <Route path="/" element={<FilterPanel />} /> } */}
-        <Route path="/" element={<CommissionerList />} />
+         { <Route path="/" element={<FilterPanel />} /> } 
+        <Route path="/commissioner" element={<CommissionerList />} />
+        <Route path="search" element={<SearchOfficials />} />
       </Routes>
     </Router>
   );
---
+++ my-app/src/components/Searcofficials.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// import './SearchOfficials.css';
+import icon from '../assets/images/simple-line-icons_people.png';
+
+
+const SearchOfficials = () => {
+  return (
+    <div className="search-officials-container">
+      <h2 className="search-title">Search Officials</h2>
+      <div className="search-content">
+        <div className="icon-placeholder">
+          <img
+            src={icon}
+            alt="User icon"
+          />
+          <p>No Activity yet</p>
+        </div>
+        <button className="search-button">Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchOfficials;
